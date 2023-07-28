6d6f0f2d26f3f87c10b49dbcbba8685fbb451807
Promit Bhattacharjee <cse_1932020048@lus.ac.bd>
2023-07-29 05:46:56 +0600

    5th

--- database.php
+++ database.php
@@ -5,9 +5,9 @@ $password="";
 $databaseName="makeatry";
 $connect = mysqli_connect($hostName,$userName,$password,$databaseName);
 if(!$connect){
-    die("not");
+    die("DataBase Error");
 }
 else{
-echo "Connected";
+// echo "Connected";
 }
 ?>
---
+++ signUpAction.php
@@ -0,0 +1,6 @@
+<?php
+if($_SERVER["REQUEST"]=="POST")
+{
+    $_POST['']
+}
+?>
